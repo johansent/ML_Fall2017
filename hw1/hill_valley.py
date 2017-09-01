326242391dae998efd3e2b69879749642b1d01b4
Kyle <sha11005@byui.edu>
2017-09-01 13:31:03 -0400

    redid filepath to work on my machine

--- hw1/hill_valley.py
+++ hw1/hill_valley.py
@@ -7,11 +7,14 @@ Created on Tue Aug 29 12:41:24 2017
 
 # hill-valley data
 from import_data import import_data
-
-X, Y, Xtest, Ytest = import_data('X.dat', 'Xtest.dat', 'Y.dat', 'Ytest.dat')
-
-
 from sklearn import tree
+import sklearn
+import numpy as np
+
+
+X, Y, Xtest, Ytest = import_data('hill_valley','X.dat', 'Xtest.dat', 'Y.dat', 'Ytest.dat')
+
+
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
     
--- hw1/import_data.py
+++ hw1/import_data.py
@@ -5,17 +5,20 @@ Created on Tue Aug 29 13:57:52 2017
 @author: joh10
 """
 import pandas
+import os
+import os.path
 
-def import_data(train1, test1, train2 = None, test2 = None):
-    filepath = 'C:/Users/joh10/Desktop/FSU/FA17/5635/hw1/'
+def import_data(path, train1, test1, train2 = None, test2 = None, col = -1):
+    #filepath = 'C:/Users/joh10/Desktop/FSU/FA17/5635/hw1/Data' + path
+    filepath = os.pardir + '\\Data\\' + path + '\\'
     
 ### NOTE: add header, otherwise we miss first row    
-    
     train_data = pandas.read_table(filepath + train1, sep = ' ')
     if train2 != None:
         train2_data = pandas.read_table(filepath + train2, sep = ' ')
     #else:
         # separate training data, if not already
+        
         
 
     
