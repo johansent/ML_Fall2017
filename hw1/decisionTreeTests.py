6057b21a9fc729a51a4055ea879b0b9cc5b22641
Kyle <sha11005@byui.edu>
2017-09-01 14:46:38 -0400

    added plot

--- hw1/decisionTreeTests.py
+++ hw1/decisionTreeTests.py
@@ -7,6 +7,7 @@ Created on Fri Sep 01 13:01:27 2017
 
 from import_data import import_data
 from sklearn import tree
+import matplotlib.pyplot as plt
 import sklearn
 import numpy as np
 
@@ -14,14 +15,25 @@ X, Y, Xtest, Ytest = import_data('madelon', 'madelon_train.data', 'madelon_valid
 X.drop(X.columns[len(X.columns)-1], axis=1, inplace=True)
 Xtest.drop(Xtest.columns[len(Xtest.columns)-1], axis=1, inplace=True)  #conner.xyz  at https://stackoverflow.com/questions/20517650/how-to-delete-the-last-column-of-data-of-a-pandas-dataframe
 
-
-for d in range(1,13):
+errorsTrain = []
+errorsTest = []
+D = range(1,12)
+for d in D:
     clf = tree.DecisionTreeClassifier(max_depth = d)
     clf = clf.fit(X, Y)
     
-    m = clf.score(Xtest, Ytest)
-    print(m)
-    Ypred = clf.predict(Xtest)
-    Ypred - Ytest
-    print(sklearn.metrics.confusion_matrix(np.array(Ytest), Ypred), "\n")
+    error = 1 - clf.score(X, Y)
+    errorsTrain.append(error)
+    
+    error = 1-clf.score(Xtest, Ytest)
+    errorsTest.append(error)
+    
+plt.plot(D, errorsTrain, label = 'Training Error')
+plt.plot(D, errorsTest, label = 'Test Error')
+plt.legend()
+plt.show()
+
+    
+    
+
     
