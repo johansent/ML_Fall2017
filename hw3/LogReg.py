5675631bdc203d9723b5cdde5057efad5b1a4a1d
LAPTOPTOM\joh10 <johansen.thomas78@gmail.com>
2017-09-21 15:25:34 -0400

    add to hw3

--- hw3/LogReg.py
+++ hw3/LogReg.py
@@ -7,7 +7,6 @@ Created on Mon Sep 18 11:29:36 2017
 
 from import_data import import_data
 from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier as RFC
 import pandas
 import matplotlib.pyplot as plt
 import sklearn
@@ -25,17 +24,43 @@ Xtest = preprocessing.scale(Xtest)
 #initialize variables
 Nrow = len(X)
 Ncol = len(X[0])
-w = [0]*Ncol
+w = [0]*(Ncol + 1)
 
 learningRate = 1
 lam = .001
+        
 
-for k in range(300):
+for i in range(300):
+    for k in range(Ncol + 1):
+        j = 0
+        dL = 0
+        for r in X:
+            exp = np.exp(w[0] + np.dot(np.matrix(w[1:len(w)]), np.transpose(np.matrix(r))))
+            dLnew = r[j]*(Y[j] - exp/(1 - exp))
+            dL = dL + dLnew
+            j = j + 1
+        w[k] = w[k] - learningRate*lam*w[k] + learningRate*dL/Nrow
+        
+     
+     
+for k in range(1):
+    j = 0
     for r in X:
-        for c in r:
-            i = i + 1
-    if i > 10:
-        break
+#        print(np.dot(np.matrix(w[1:len(w)]), np.transpose(np.matrix(r))))
+        exp = np.exp(w[0] + np.dot(np.matrix(w[1:len(w)]), np.transpose(np.matrix(r))))
+        newdL = np.array(r)*(Y[j] - exp/(1 - exp))
+#        dL = dL + newdL
+        j = j + 1
+        #for c in r:
+#        i = i + 1
+#        print(i)
+#        print(type(r))
+#        print(np.shape(r))
+#        tmp = np.transpose(np.matrix(r))
+#        print(type(tmp))
+#        print(np.shape(tmp))
+#        if i == 1:
+#            break
     
     
 
--- hw3/import_data.py
+++ hw3/import_data.py
@@ -5,12 +5,10 @@ Created on Tue Aug 29 13:57:52 2017
 @author: joh10
 """
 import pandas
-import os
-import os.path
 
 def import_data(path, train1, test1, train2 = None, test2 = None, col = -1, head = 0):
-    #filepath = 'C:/Users/joh10/Desktop/FSU/FA17/5635/hw1/Data' + path
-    filepath = os.pardir + '\\Data\\' + path + '\\'
+    filepath = 'C:/Users/joh10/Desktop/FSU/FA17/5635/git/Data/' + path + '/'
+    #filepath = os.pardir + '\\Data\\' + path + '\\'
     
 ### NOTE: add header, otherwise we miss first row    
     train_data = pandas.read_table(filepath + train1, sep = ' ', header = head)
