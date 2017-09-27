8480944c9c93ef2682c5384a745006b95afed2cb
Kyle <sha11005@byui.edu>
2017-09-26 22:23:36 -0400

    Added report and cleaned up code a little for submission

--- hw3/LogReg.py
+++ hw3/LogReg.py
@@ -13,62 +13,24 @@ import sklearn
 import numpy as np
 
 def updateWeights(X,X1,Y,w, Ncol,Nrow, learningRate, lam):
-#    X = preprocessing.scale(X)
-#    Y = [0 if y <= 0 else 1 for y in np.array(Y)]
-#    X1 = np.array(np.c_[np.ones((len(X), 1)), np.matrix(X)])
-#    #initialize variables
-#    Nrow = len(X)
-#    Ncol = len(X1[0])
-#
-#    learningRate = .01
-#    lam = .001
-        
-
     tmp = w[1:Ncol]
     product = np.dot(X,tmp)
-    #print('product', product)
     shiftedValue = w[0] + product
-    #print('shiftedValue',shiftedValue)
     expValue = np.exp(shiftedValue)
-    #print('expValue', expValue)
     ratio = expValue / (1 + expValue)
-    #print('ratio',ratio)
     error = Y - ratio
-    #print('error',error)
-    
-    
     dLnew = np.dot(np.transpose(X1),error)
-    #print('dLnew',dLnew)
     w = w + learningRate * (-lam*w + dLnew/Nrow)
-        
-#        dL = np.sum(dLnew)
-#        print('Dl', dL)
-#        w = w - learningRate * (lam*w + dL/Nrow)
-           
-        #for k in range(Ncol):
-            #dL = sum(dLnew)
-#        for j in range(Nrow):
-##            if j == 0:
-##                tmp = w[0] + np.dot(w[1:len(w)], r[1:len(r)])
-##                print(tmp)
-#            #e_dotprod = np.exp(w[0] + np.dot(tmp, r[1:Nrow]))
-##            print(np.shape(w))
-#            dLnew = r[k]*(Y[0][j] - e_dotprod/(1 + e_dotprod))
-##            print(np.shape(w))
-#            dL = dL + dLnew
-            #w[k] = w[k] - learningRate*lam*w[k] + learningRate*dL/Nrow
-        #print(w[1])
     return w
 
 def Test(w, X, Y):
-    #Add column of 1s
+    #nomralize data
     X = preprocessing.scale(X)
+    #Add column of 1s
     X = np.array(np.c_[np.ones((len(X), 1)), np.matrix(X)])
     Y = [0 if y <= 0 else 1 for y in np.array(Y)]
     wx = 1/(1+np.exp(-1 * np.dot(X, w)))
     Ypredict = [0 if x < .5 else 1 for x in wx]
-    
-    
     results = np.array(Y) - np.array(Ypredict)
     return sum(abs(results))/len(Y)
     
@@ -77,9 +39,11 @@ def Plot(x,y1,y2,title,legendLoc = 1):
     plt.plot(x,y1,label = 'Test Error')
     plt.plot(x,y2,label = 'Training Error')
     plt.legend(loc = legendLoc)
+    plt.xlabel('iteration count')
+    plt.ylabel('Misclassification Error')
     plt.show()
     
-def TrainWeights(X,Y,Xtest,Ytest,k):
+def TrainWeights(X,Y,Xtest,Ytest,k, learnRate = .01):
     X = preprocessing.scale(X)
     Y = [0 if y <= 0 else 1 for y in np.array(Y)]
     X1 = np.array(np.c_[np.ones((len(X), 1)), np.matrix(X)])
@@ -87,7 +51,7 @@ def TrainWeights(X,Y,Xtest,Ytest,k):
     Nrow = len(X)
     Ncol = len(X1[0])
 
-    learningRate = .01
+    learningRate = learnRate
     lam = .001
     
     w = np.array([0]*(Ncol))
@@ -115,11 +79,11 @@ X, Y, Xtest, Ytest = import_data('gisette', 'gisette_train.data', 'gisette_valid
 X.drop(X.columns[len(X.columns)-1], axis=1, inplace=True)
 Xtest.drop(Xtest.columns[len(Xtest.columns)-1], axis=1, inplace=True)  #conner.xyz  at https://stackoverflow.com/questions/20517650/how-to-delete-the-last-column-of-data-of-a-pandas-dataframe
 
-niter = 30
-y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter)
+niter = 50
+y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter,.1)
 
 Plot(range(niter), y1, y2, 'Gisette Errors')
-min_table['Gisette      '] = [min(y1), min(y2)]
+min_table['Gisette'] = [min(y1), min(y2)]
 
 
 #Arcene Test
@@ -127,12 +91,12 @@ X, Y, Xtest, Ytest = import_data('arcene', 'arcene_train.data', 'arcene_valid.da
 X.drop(X.columns[len(X.columns)-1], axis=1, inplace=True)
 Xtest.drop(Xtest.columns[len(Xtest.columns)-1], axis=1, inplace=True)
 
-niter = 30
-y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter)
+niter = 100
+y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter, .001)    
 
 Plot(range(niter), y1, y2, 'Arcene Errors')
 #Plot(K,eTest,eTrain,'Arcene')
-min_table['Arcene      '] = [min(y1), min(y2)]
+min_table['Arcene'] = [min(y1), min(y2)]
 
 # Madelon
 X, Y, Xtest, Ytest = import_data('madelon', 'madelon_train.data', 'madelon_valid.data','madelon_train.labels', 'madelon_valid.labels', head = None)
@@ -143,19 +107,24 @@ niter = 500
 y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter)
 
 Plot(range(niter), y1, y2, 'Madelon Errors', 3)
-min_table['Madelon      '] = [min(y1), min(y2)]
+min_table['Madelon'] = [min(y1), min(y2)]
 
 
 # Hill/Valley
 X, Y, Xtest, Ytest = import_data('hill_valley', 'X.dat', 'Xtest.dat','Y.dat', 'Ytest.dat', head = None)
 
-niter = 300
+niter = 3000
 y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter)
 
 Plot(range(niter), y1, y2, 'Hill/Valley Errors', 3)
-min_table['Hill/Valley      '] = [min(y1), min(y2)]
+min_table['Hill/Valley'] = [min(y1), min(y2)]
 
-print(min_table)
+#Summary Of Results
+print('{0:17s} {1:12s} {2:12s}'.format('Data name', 'Test Error', 'Training Error'))
+print('---------------------------------------')
+for k in min_table.keys():
+    print('{0:12s} {1:13f} {2:12f}'.format(k, min_table[k][0], min_table[k][1]))
+    
 stop = timeit.default_timer()
 print(stop - start)
 
