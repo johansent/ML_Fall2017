0266a4fd8338deadea4eaa1376d150c7a22180ad
Kyle <sha11005@byui.edu>
2017-10-19 14:23:29 -0400

    Fixed the build, havent checked accuracy

--- hw6/FSA.py
+++ hw6/FSA.py
@@ -24,25 +24,57 @@ from import_data import import_data
 #    
 #    return Xnew, Xtest_new
 
-def updateWeights(X,X1,Y,w, Ncol,Nrow, learningRate, lam):
-    tmp = w[1:Ncol]
-    product = np.dot(X,tmp)
-    shiftedValue = w[0] + product
-    expValue = np.exp(shiftedValue)
-    ratio = expValue / (1 + expValue)
-    error = Y - ratio
-    dLnew = np.dot(np.transpose(X1),error)
-    w = w + learningRate * (-lam*w + dLnew/Nrow)
+def updateWeights(X,Y,w, Ncol,Nrow, learningRate, s):
+    #tmp = w[1:Ncol]
+#    product = np.dot(X,w)
+#    divisor = 2 + product*product - 2 * np.transpose(Y * np.transpose(product))
+#    Value = 2 * product * X 
+
+    sumation = [0] * Ncol
+    #print(sum(X))
+    for k, Row in X.iterrows():
+        row = np.array(Row)
+#        for r in row:
+#            print(r, type(r))
+        #print('row', np.sort(row))
+        #print('w',type(w[-1]))
+        #print(sum(w), sum(row))
+        product = Y[k] * np.dot(w,row)
+        #print(np.dot(w,row))
+        if(product < 1):
+            divisor = 2 + product*product - 2 * product
+            numerator = 2 * (product - 1)
+            value = (numerator / divisor) * row
+            sumation = sumation + value
+        
+    print(type(sumation), type(s), type(w))
+    derivative = sumation + s * w
+    w = w - learningRate * derivative
+    #print('w',w)
+    
+    
+
+    
     return w
+    
+        
+    
+    
+#    shiftedValue = w[0] + product
+#    expValue = np.exp(shiftedValue)
+#    ratio = expValue / (1 + expValue)
+#    error = Y - ratio
+#    dLnew = np.dot(np.transpose(X1),error)
+#    w = w + learningRate * (-lam*w + dLnew/Nrow)
+#    return w
 
 def Test(w, X, Y):
-    #nomralize data
-    X = preprocessing.scale(X)
     #Add column of 1s
-    X = np.array(np.c_[np.ones((len(X), 1)), np.matrix(X)])
+    #X = np.array(np.c_[np.ones((len(X), 1))X, np.matrix(X)])
     Y = [0 if y <= 0 else 1 for y in np.array(Y)]
-    wx = 1/(1+np.exp(-1 * np.dot(X, w)))
-    Ypredict = [0 if x < .5 else 1 for x in wx]
+    #wx = 1/(1+np.exp(-1 * np.dot(X, w)))
+    wx = np.dot(X,w)
+    Ypredict = [0 if x < 0 else 1 for x in wx]
     results = np.array(Y) - np.array(Ypredict)
     return sum(abs(results))/len(Y)
     
@@ -55,27 +87,40 @@ def Plot(x,y1,y2,title,legendLoc = 1):
     plt.ylabel('Misclassification Error')
     plt.show()
     
-def TrainWeights(X,Y,Xtest,Ytest,k, learnRate = .01):
-    X = preprocessing.scale(X)
-    Y = [0 if y <= 0 else 1 for y in np.array(Y)]
-    X1 = np.array(np.c_[np.ones((len(X), 1)), np.matrix(X)])
+def TrainWeights(X,Y,Xtest,Ytest,niter,k, learnRate = .01):
+    Y = [-1 if y <= 0 else 1 for y in np.array(Y)]
+    #X1 = np.array(np.c_[np.ones((len(X), 1)), np.matrix(X)])
     #initialize variables
     Nrow = len(X)
-    Ncol = len(X1[0])
+    Ncol = len(X.columns)
 
-    learningRate = learnRate
-    lam = .001
+    s = .001
     
     w = np.array([0]*(Ncol))
     
-    y1 = []
-    y2 = []
-    for i in range(k):
-        w = updateWeights(X,X1,Y,w, Ncol,Nrow, learningRate, lam)
-        y1.append(Test(w,Xtest, Ytest))
-        y2.append(Test(w,X,Y))
+    testErrors = []
+    trainingErrors = []
+    for i in range(niter):
+        Mi = getMi(Ncol, k, niter, i, u = 100)
+        print('Mi', Mi)
+        w = updateWeights(X,Y,w, Ncol,Nrow, learnRate, s)
+        w,X,Xtest,Ncol = getMBest(w, X, Xtest, Mi, Ncol)
+        testErrors.append(Test(w,Xtest, Ytest))
+        trainingErrors.append(Test(w,X,Y))
+        print(testErrors)
         
-    return y1,y2
+    return testErrors,trainingErrors
+
+def getMi(M, k, N, i, u = 100):
+    return round(k + (M - k) * max([0,(N - 2 * i)/(2 * i * u + N)]))
+    
+def getMBest(w, X, Xtest, M, Ncol):
+    best = sorted(range(len(w)), key=lambda i: w[i])[-M:]
+    worst = sorted(range(len(w)), key = lambda i: w[i])[0:(Ncol - M)]
+    w = np.array([w[i] for i in best])
+    X.drop(X.columns[worst], axis=1, inplace=True)
+    Xtest.drop(Xtest.columns[worst], axis=1, inplace=True)
+    return w, X, Xtest, len(X.columns)
     
 
 
@@ -84,52 +129,51 @@ import timeit
 start = timeit.default_timer()
 min_table = {}
 
-#Gisette Data
-    
-#Read in the Data
-X, Y, Xtest, Ytest = import_data('gisette', 'gisette_train.data', 'gisette_valid.data','gisette_train.labels', 'gisette_valid.labels', head = None)
-X.drop(X.columns[len(X.columns)-1], axis=1, inplace=True)
-Xtest.drop(Xtest.columns[len(Xtest.columns)-1], axis=1, inplace=True)  #conner.xyz  at https://stackoverflow.com/questions/20517650/how-to-delete-the-last-column-of-data-of-a-pandas-dataframe
-
-niter = 50
-y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter,.1)
-
-Plot(range(niter), y1, y2, 'Gisette Errors')
-min_table['Gisette'] = [min(y1), min(y2)]
-
-
+##Gisette Data
+#    
+##Read in the Data
+#X, Y, Xtest, Ytest = import_data('gisette', 'gisette_train.data', 'gisette_valid.data','gisette_train.labels', 'gisette_valid.labels', head = None)
+#X.drop(X.columns[len(X.columns)-1], axis=1, inplace=True)
+#Xtest.drop(Xtest.columns[len(Xtest.columns)-1], axis=1, inplace=True)  #conner.xyz  at https://stackoverflow.com/questions/20517650/how-to-delete-the-last-column-of-data-of-a-pandas-dataframe
+#
+#niter = 50
+#y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter,.1)
+#
+#Plot(range(niter), y1, y2, 'Gisette Errors')
+#min_table['Gisette'] = [min(y1), min(y2)]
+#
+#
 #Arcene Test
-X, Y, Xtest, Ytest = import_data('arcene', 'arcene_train.data', 'arcene_valid.data','arcene_train.labels', 'arcene_valid.labels', head = None, norm = True)
-X.drop(X.columns[len(X.columns)-1], axis=1, inplace=True)
-Xtest.drop(Xtest.columns[len(Xtest.columns)-1], axis=1, inplace=True)
+X, Y, Xtest, Ytest = import_data('arcene', 'arcene_train.data', 'arcene_valid.data','arcene_train.labels', 'arcene_valid.labels', head = None, norm = True, removeCol = True)
 
-niter = 100
-y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter, .001)    
+niter = 500
+y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter,100, .001)    
 
 Plot(range(niter), y1, y2, 'Arcene Errors')
 #Plot(K,eTest,eTrain,'Arcene')
 min_table['Arcene'] = [min(y1), min(y2)]
 
-# Madelon
-X, Y, Xtest, Ytest = import_data('madelon', 'madelon_train.data', 'madelon_valid.data','madelon_train.labels', 'madelon_valid.labels', head = None)
-X.drop(X.columns[len(X.columns)-1], axis=1, inplace=True)
-Xtest.drop(Xtest.columns[len(Xtest.columns)-1], axis=1, inplace=True)
+## Madelon
+#X, Y, Xtest, Ytest = import_data('madelon', 'madelon_train.data', 'madelon_valid.data','madelon_train.labels', 'madelon_valid.labels', head = None)
+#X.drop(X.columns[len(X.columns)-1], axis=1, inplace=True)
+#Xtest.drop(Xtest.columns[len(Xtest.columns)-1], axis=1, inplace=True)
+#
+#niter = 500
+#y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter)
+#
+#Plot(range(niter), y1, y2, 'Madelon Errors', 3)
+#min_table['Madelon'] = [min(y1), min(y2)]
+#
+#
+## Hill/Valley
+#X, Y, Xtest, Ytest = import_data('hill_valley', 'X.dat', 'Xtest.dat','Y.dat', 'Ytest.dat', head = None)
+#
+#niter = 3000
+#y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter)
+#
+#Plot(range(niter), y1, y2, 'Hill/Valley Errors', 3)
+#min_table['Hill/Valley'] = [min(y1), min(y2)]
 
-niter = 500
-y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter)
-
-Plot(range(niter), y1, y2, 'Madelon Errors', 3)
-min_table['Madelon'] = [min(y1), min(y2)]
-
-
-# Hill/Valley
-X, Y, Xtest, Ytest = import_data('hill_valley', 'X.dat', 'Xtest.dat','Y.dat', 'Ytest.dat', head = None)
-
-niter = 3000
-y1, y2 = TrainWeights(X,Y,Xtest,Ytest,niter)
-
-Plot(range(niter), y1, y2, 'Hill/Valley Errors', 3)
-min_table['Hill/Valley'] = [min(y1), min(y2)]
 
 #Summary Of Results
 print('{0:17s} {1:12s} {2:12s}'.format('Data name', 'Test Error', 'Training Error'))
--- import_data.py
+++ import_data.py
@@ -6,10 +6,11 @@ Created on Tue Aug 29 13:57:52 2017
 """
 import pandas
 import numpy as np
+import os
 
-def import_data(path, train1, test1, train2 = None, test2 = None, col = -1, head = 0, norm = False):
-    filepath = 'C:/Users/joh10/Desktop/FSU/FA17/5635/git/Data/' + path + '/'
-    #filepath = os.pardir + '\\Data\\' + path + '\\'
+def import_data(path, train1, test1, train2 = None, test2 = None, removeCol = False, head = 0, norm = False):
+    #filepath = 'C:/Users/joh10/Desktop/FSU/FA17/5635/git/Data/' + path + '/'
+    filepath = os.pardir + '\\Data\\' + path + '\\'
     
 ### NOTE: add header, otherwise we miss first row    
     train_data = pandas.read_table(filepath + train1, sep = ' ', header = head)
@@ -27,6 +28,10 @@ def import_data(path, train1, test1, train2 = None, test2 = None, col = -1, head
     #else:
         ## separate test data, if not already
         
+    if removeCol:
+        train_data.drop(train_data.columns[len(train_data.columns)-1], axis=1, inplace=True)
+        test_data.drop(test_data.columns[len(test_data.columns)-1], axis=1, inplace=True)
+        
     if normalize:
         train_data, test_data = normalize(train_data, test_data)
         
@@ -35,12 +40,29 @@ def import_data(path, train1, test1, train2 = None, test2 = None, col = -1, head
     
 def normalize(X, Xtest):
     npX = np.array(X)
-    mns = np.mean(npX, axis = 0)
+    npXt = np.array(Xtest)
     std = np.std(npX, axis = 0)
+    zeros = np.array(np.where(std == 0))
+#    print('zeros', np.shape(zeros), zeros)
+    zeros.sort()
+    zeros = zeros[::-1]
+#    print('zeros', np.shape(zeros), zeros)
+#    print(std[zeros[0][0]])
+    N = len(zeros[0])
+    for i in range(N):
+        #print(zeros[0][i])
+#        print(i)
+        npX = np.delete(npX,zeros[0][N-i-1] , 1)
+        npXt = np.delete(npXt,zeros[0][N-i-1],1)
+        std = np.delete(std, zeros[0][N-i-1])
+    mns = np.mean(npX, axis = 0)
     Xnew = (npX - mns)/std
-    Xtest_new = (np.array(Xtest) - mns)/std
+    Xtest_new = (npXt - mns)/std
     Xnew = pandas.core.frame.DataFrame(Xnew)
     Xtest_new = pandas.core.frame.DataFrame(Xtest_new)
+    #print(Xnew)
+    
+    #print(np.shape(npX), np.shape(npXt))
     
     return Xnew, Xtest_new    
     
